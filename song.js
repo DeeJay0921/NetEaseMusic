ba4a80152c3f89f706e767b0d4d319e992502e9c
DeeJay <1018805743@qq.com>
2017-10-01 15:31:26 +0800

    accomplish lyric scroll

--- song.js
+++ song.js
@@ -17,7 +17,6 @@ $(function () {
         $('.songTitle').text(name)
         // console.log(name,lyric)
         parseLyric(lyric)
-        console.log(lyric)
     }
     
     function parseLyric(lyric) {
@@ -64,6 +63,34 @@ $(function () {
             audio.play()
             $('.disc-container').addClass('playing')
         })
+        setInterval(()=> {
+            let seconds = audio.currentTime
+            let minutes = ~~(seconds / 60)
+            let left = seconds - minutes*60
+            let time =  `${pad(minutes)}:${pad(left)}`
+            // console.log(time)//拿到currentTime
+            let $p = $('.lines>p')
+            let $whichLine
+            for (let i = 0;i < $p.length; i ++) {
+                if( $p[i+1] !== undefined && $p.eq(i).attr('data-time') < time && $p.eq(i+1).attr('data-time') > time) { //超过当前时间但是没到下一句的时间时，显示当前行
+                    $whichLine = $p.eq(i)
+                }
+            }
+            if ($whichLine) {
+                $whichLine.addClass('active').prev().removeClass('active')
+                let top = $whichLine.offset().top
+                let linesTop = $('.lines').offset().top
+                let delta = top - linesTop - $('.lyric').height()/3
+                $('.lines').css(
+                    'transform', `translateY(-${delta}px)`
+                )
+                // console.log(top,linesTop)
+            }
+        },500)
+    }
+
+    function pad(number) {
+        return number >= 10 ? number + '' : '0' + number
     }
 
     $.get('/lyric.json').then(function (response) {
